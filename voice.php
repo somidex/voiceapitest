a03cc392bb9b50799741a8244126e7c111da9c12
Dexter Loor <dexter.loor@searchoptmedia.com>
2016-03-05 16:54:36 +0800

    Call Script.

--- voice.php
+++ voice.php
@@ -1,6 +1,18 @@
 <?php
 
-call('+639173176758');
-say("Hi! Welcome to Tropo");
+//$url = 'https://api.tropo.com/1.0/sessions?action=create';
+//$voiceAppId = '816';
+//$voiceToken = '464b4a6f4446714c616e48686a4e4f714c7054574c6f6e45526b63486f6b4468697663524f6e5848696a4e4e';
+//$numberToDial = '639173176758';
+
+call('+639173176758', {
+	"timeout" => 60,
+	"onAnswer" => "answerFcn"
+});
+
+function answerFcn($event) {
+	say('Hi! Welcome to Tropo!');
+	_log("Call complete.");
+}
 
 ?>
