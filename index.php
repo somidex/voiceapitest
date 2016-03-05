bec789cd9c2234d87c47df1c597c941ad6cfc5a4
Dexter Loor <dexter.loor@searchoptmedia.com>
2016-03-05 16:04:48 +0800

    Callback URLs established.

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Test Voice App</title>
+</head>
+<body>
+	
+	<div class="wrap">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>Test Application for Voice API</h1>
+				<a href="/voice.php">Execute Voice</a>
+			</div>
+		</div>
+	</div>
 
-echo "Test";
-
-?>
+</body>
+</html>
